59c2a593c2af116571ec9f53ce286e568864bee7
Ikkotz <ikerhc@gmail.com>
2025-10-24 00:14:45 +0200

    Print cleanup

--- main.py
+++ main.py
@@ -4,15 +4,8 @@ from utils import tablero, colocar_barcos_aleatorios, turno_juego
 tablero_jugador = tablero()
 tablero_rival = tablero()
 
-print("Tablero jugador:")
-print(tablero_jugador)
-
 # Colocar los barcos
 flota = [4, 3, 3, 2, 2, 2]
 colocar_barcos_aleatorios(tablero_jugador, flota)
-
-print("Tablero jugador con barcos:")
-print(tablero_jugador)
-
 # Iniciar el sistema de turnos
 turno_juego()
